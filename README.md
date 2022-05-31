1cbffc8baf6498c4bf20d13233f36753efa0ef8c
marth141 <ncasados@live.com>
2022-05-31 14:03:41 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Google SSO Example
+
+To show an example of how to setup SSO in Google.
---
+++ config/dev.secret.exs
@@ -0,0 +1,6 @@
+import Config
+
+# Google OAuth creds #SSO
+config :ueberauth, Ueberauth.Strategy.Google.OAuth,
+  client_id: System.get_env("GOOGLE_CLIENT_ID"),
+  client_secret: System.get_env("GOOGLE_CLIENT_SECRET")
